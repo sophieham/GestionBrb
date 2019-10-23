419ff561ed27effca32aa06c3860f8ceec68a2c6
Sophie <44404051+sophiehm28@users.noreply.github.com>
2019-10-23 23:14:24 +0200

    test

--- GestionBrb/src/gestionBrb/model/Administrateur.java
+++ GestionBrb/src/gestionBrb/model/Administrateur.java
@@ -1,19 +1,7 @@
 package gestionBrb.model;
 
-
-/**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
- */
-
 public class Administrateur extends Utilisateurs
 {
-	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 */
 	public Administrateur(){
 		super();
 	}
