bb276a3aabebf88b3f5987a30a25e0ef5ed21c01
Sophie <sophie.hm@protonmail.com>
2019-10-29 18:27:52 +0100

    maj dbutil

--- Application/src/gestionbrb/util/bddUtil.java
+++ Application/src/gestionbrb/util/bddUtil.java
@@ -2,18 +2,25 @@ package gestionbrb.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.cj.x.protobuf.MysqlxSql.StmtExecuteOkOrBuilder;
 
 public class bddUtil {
 	private final static String JDBCDriver = "com.mysql.cj.jdbc.Driver";
 	private final static String url = "jdbc:mysql://localhost:3306/gestionbrb";
 	private final static String user = "root";
 	private final static String password = "";
+	private static Connection conn = null;
+	private static Statement stmt;
+	private static ResultSet rs;
 
-	public static void bddConnexion() throws SQLException, ClassNotFoundException {
+	public static void dbConnect() throws SQLException, ClassNotFoundException {
 		try {
 		Class.forName(JDBCDriver);
-		Connection conn = DriverManager.getConnection(url, user, password);
+		conn = DriverManager.getConnection(url, user, password);
 		System.out.println("Connexion reussie !");
 		}
 		catch(SQLException e) {
@@ -26,8 +33,56 @@ public class bddUtil {
 		}
 
 	}
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		bddConnexion();
+	static void dbDisconnect() throws SQLException {
+		if (conn != null) {
+			try {
+			conn.close();
+			}
+			catch(SQLException e) {
+			e.printStackTrace();
+			}
+		}
+		
 	}
+	
+	static void dbExecuteQuery(String query) throws ClassNotFoundException, SQLException {
+		dbConnect();
+		try {
+		stmt = conn.createStatement();
+		stmt.executeUpdate(query);
+		System.out.println("Reqûete effectuée!");
+		}
+		catch (SQLException e) {
+			System.out.println("Erreur dans le code sql");
+			e.printStackTrace();
+		}
+		finally {
+			dbDisconnect();
+			stmt.close();
+		}
+	}
+	static void dbQuery(String query) throws ClassNotFoundException, SQLException {
+		dbConnect();
+		try {
+		stmt = conn.createStatement();
+		rs = stmt.executeQuery(query);
+		while (rs.next()) {
+			int i = rs.getInt("idType");
+			String s = rs.getString("nom");
+			System.out.println(i+" "+s);
+            
+        }
+		System.out.println("Reqûete effectuée!");
+		}
+		catch (SQLException e) {
+			System.out.println("Erreur dans le code sql"+e);
+		}
+		finally {
+			dbDisconnect();
+			stmt.close();
+			rs.close();
+		}
+	}
+
 
 }
