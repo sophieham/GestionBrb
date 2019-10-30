36349bad044a599e078e04a3b98f279400b3489d
Roman <astro.romon@gmail.com>
2019-10-30 13:47:53 +0100

    Re-modification de Table pour JavaFX

--- Application/src/gestionbrb/model/Table.java
+++ Application/src/gestionbrb/model/Table.java
@@ -1,5 +1,6 @@
 package gestionbrb.model;
 
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
@@ -8,61 +9,97 @@ import javafx.beans.property.StringProperty;
 
 public class Table
 {
-	private int idTable;
-	private int nbCouvertsMin;
-	private String  demandeSpe;
+	private final IntegerProperty idTable;
+	private final IntegerProperty nbCouvertsMin;
+	private final StringProperty  demandeSpe;
 	private Commande commande;
-	private int nbCouvertsMax;
-	private boolean estOccupe;
+	private final IntegerProperty nbCouvertsMax;
+	private BooleanProperty estOccupe;
 	
 	public Table(int idTable, int nbCouvertsMin, int nbCouvertsMax, String demandeSpe, boolean estOccupe){
-		this.idTable =idTable;
-		this.nbCouvertsMin = (nbCouvertsMin);
-		this.nbCouvertsMax = (nbCouvertsMax);
-		this.demandeSpe= (demandeSpe);
-		this.estOccupe= (estOccupe);
+		this.idTable = new SimpleIntegerProperty(idTable);
+		this.nbCouvertsMin = new SimpleIntegerProperty(nbCouvertsMin);
+		this.nbCouvertsMax = new SimpleIntegerProperty(nbCouvertsMax);
+		this.demandeSpe= new SimpleStringProperty(demandeSpe);
+		this.estOccupe= new SimpleBooleanProperty(estOccupe);
 	}
 
-	public int getIdTable() {
-		return idTable;
+	public final IntegerProperty idTableProperty() {
+		return this.idTable;
 	}
 	
-	public void setIdTable(int idTable) {
-		this.idTable = idTable;
+
+	public final int getIdTable() {
+		return this.idTableProperty().get();
 	}
 	
-	public int getNbCouvertsMin() {
-		return nbCouvertsMin;
+
+	public final void setIdTable(final int idTable) {
+		this.idTableProperty().set(idTable);
 	}
 	
-	public void setNbCouvertsMin(int nbCouvertsMin) {
-		this.nbCouvertsMin=nbCouvertsMin;
-	}
 
-
-	public int getNbCouvertsMax() {
-		return nbCouvertsMax;
-	}
-
-	public void setNbCouvertsMax(int nbCouvertsMax) {
-		this.nbCouvertsMax=nbCouvertsMax;
+	public final IntegerProperty nbCouvertsMinProperty() {
+		return this.nbCouvertsMin;
 	}
 	
-	public String getDemandeSpe() {
-		return demandeSpe;
-	}
 
-	public void setDemandeSpe(String demandeSpe) {
-		this.demandeSpe = demandeSpe;
+	public final int getNbCouvertsMin() {
+		return this.nbCouvertsMinProperty().get();
 	}
 	
-	public boolean getEstOccupe() {
-		return estOccupe;
-	}
 
-	public void setEstOccupe(boolean estOccupe) {
-		this.estOccupe = estOccupe;
+	public final void setNbCouvertsMin(final int nbCouvertsMin) {
+		this.nbCouvertsMinProperty().set(nbCouvertsMin);
 	}
+	
+
+	public final StringProperty demandeSpeProperty() {
+		return this.demandeSpe;
+	}
+	
+
+	public final String getDemandeSpe() {
+		return this.demandeSpeProperty().get();
+	}
+	
+
+	public final void setDemandeSpe(final String demandeSpe) {
+		this.demandeSpeProperty().set(demandeSpe);
+	}
+	
+
+	public final IntegerProperty nbCouvertsMaxProperty() {
+		return this.nbCouvertsMax;
+	}
+	
+
+	public final int getNbCouvertsMax() {
+		return this.nbCouvertsMaxProperty().get();
+	}
+	
+
+	public final void setNbCouvertsMax(final int nbCouvertsMax) {
+		this.nbCouvertsMaxProperty().set(nbCouvertsMax);
+	}
+	
+
+	public final BooleanProperty estOccupeProperty() {
+		return this.estOccupe;
+	}
+	
+
+	public final boolean isEstOccupe() {
+		return this.estOccupeProperty().get();
+	}
+	
+
+	public final void setEstOccupe(final boolean estOccupe) {
+		this.estOccupeProperty().set(estOccupe);
+	}
+	
+
+	
 	
 	
 }
