4525ff369ffc6c1dff45a1c6b4c4ce00b57baf51
Sophie <44404051+sophiehm28@users.noreply.github.com>
2019-11-11 15:55:44 +0100

    Quelques modifications mineures

---
+++ Application/src/gestionbrb/vue/CommandeControleur.java
@@ -0,0 +1,49 @@
+package gestionbrb.vue;
+
+import gestionbrb.controleur.FonctionsControleurs;
+import gestionbrb.model.Commande;
+import gestionbrb.model.Produit;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class CommandeControleur extends FonctionsControleurs {
+	@FXML
+	private Label infoTable;
+	@FXML
+	private TableView<Produit> tableRecap;
+	@FXML
+	private TableColumn<Produit, String> colonneProduit;
+	@FXML
+	private TableColumn<Produit, Number> colonnePrix;
+	@FXML
+	private TableColumn<Produit, Number> colonneQte;
+	@FXML
+	private Label totalProduit;
+	@FXML
+	private Label totalPrix;
+	@FXML
+	private Label totalQte;
+	@FXML
+	private TabPane typeProduit;
+	
+	DemarrerCommandeControleur parent;
+	
+	public CommandeControleur() {
+	}
+	
+	private void initialize() {
+		/*colonneProduit.setCellValueFactory(cellData -> cellData.getValue().nomProperty());
+		colonnePrix.setCellValueFactory(cellData -> cellData.getValue().prixProperty());
+		colonneQte.setCellValueFactory(cellData -> cellData.getValue().qteProperty());*/
+	}
+
+	public void setParent(DemarrerCommandeControleur parent) {
+		this.parent = parent;		
+	}
+	
+	
+
+}
