4dab9d68b8b07fc67b6a3c0e1bf9caa1aa86ea43
Tayriss <56788812+Tayriss@users.noreply.github.com>
2019-11-12 18:42:15 +0100

    Gestion ingredients/produit presque fini
    Manque quelques ajustements / requetes sql

--- Application/src/gestionbrb/IngredientsProduits.java
+++ Application/src/gestionbrb/IngredientsProduits.java
@@ -7,8 +7,9 @@ import java.sql.SQLException;
 import gestionbrb.model.Ingredients;
 import gestionbrb.model.Produit;
 import gestionbrb.util.bddUtil;
-import gestionbrb.vue.IngredientsControleur;
+import gestionbrb.vue.IngredientsProduitsControleur;
 import gestionbrb.vue.ModifierIngredientsControleur;
+import gestionbrb.vue.ModifierProduitsControleur;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -28,14 +29,19 @@ public class IngredientsProduits extends Application {
 		try {
 			Connection conn = bddUtil.dbConnect();
 			ResultSet rs = conn.createStatement().executeQuery("select * from ingredients");
+			ResultSet res = conn.createStatement().executeQuery("select * from produit");
 			while (rs.next()) {
 				listeingredients.add(new Ingredients(rs.getInt("idIngredient"), rs.getString("nomIngredient"), rs.getInt("prixIngredient"), rs.getInt("qteRestante"), null));
 			}
+			rs.close();
+			while (res.next()) {
+				listeproduits.add(new Produit(res.getInt("idProduit"), res.getString("nom"),res.getInt("qte"),res.getString("description"),res.getInt("prix"), null));
+			}
+			res.close();
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
 	public static ObservableList<Ingredients> getIngredientsData() {
 		return listeingredients;
 	}
@@ -59,7 +65,7 @@ public class IngredientsProduits extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.show();
 
-			IngredientsControleur controller = loader.getController();
+			IngredientsProduitsControleur controller = loader.getController();
 			controller.setMainApp(this);
 
 		} catch (IOException e) {
@@ -95,6 +101,35 @@ public class IngredientsProduits extends Application {
 				return false;
 			}
 	} 
+	public boolean fenetreModificationn(Produit produit) throws ClassNotFoundException, SQLException {
+		try {
+			// Charge le fichier fxml et l'ouvre en pop-up
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(IngredientsProduits.class.getResource("vue/ModifierProduits.fxml"));
+			AnchorPane page = (AnchorPane) loader.load();
+
+			// Crée une nouvelle page
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Gestion liste ingredients");
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.initOwner(primaryStage);
+			Scene scene = new Scene(page);
+			dialogStage.setScene(scene);
+
+			// Définition du controleur pour la fenetre
+			ModifierProduitsControleur controller = loader.getController();
+			controller.setDialogStage(dialogStage);
+			controller.setProduit(produit);
+
+			// Affiche la page et attend que l'utilisateur la ferme.
+			dialogStage.showAndWait();
+
+			return controller.isOkClicked();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+} 
 	public Stage getPrimaryStage() {
         return primaryStage;
 	}
