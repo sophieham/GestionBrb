1e53e047572e42f0a5bd92fead256cdc27b7d3f2
DLlllxx <56820049+DLlllxx@users.noreply.github.com>
2019-11-14 08:39:39 +0100

    Add files via upload

---
+++ Application/src/gestionbrb/vue/GestionStockServ.java
@@ -0,0 +1,32 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class GestionStockServ extends Application{
+
+	@Override
+	public void start(Stage stage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(GestionStockAdminController.class.getResource("/application/GestionStockServ.fxml"));
+			AnchorPane tablesOverview = (AnchorPane) loader.load();
+
+			Scene scene = new Scene(tablesOverview);
+			stage.setTitle("GestionStock_Serveur");
+			stage.setScene(scene);
+			stage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
