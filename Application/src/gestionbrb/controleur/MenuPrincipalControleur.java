47f6e7f3ed065a337b92469acd8869d3a48db7e1
Sophie <sophie.hm@protonmail.com>
2019-11-27 19:10:03 +0100

    Rajout des getters & setters

--- Application/src/gestionbrb/controleur/MenuPrincipalControleur.java
+++ Application/src/gestionbrb/controleur/MenuPrincipalControleur.java
@@ -1,9 +1,5 @@
 package gestionbrb.controleur;
 
-import java.util.Locale;
-import java.util.ResourceBundle;
-
-import gestionbrb.vue.MenuControleur;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -19,16 +15,21 @@ import javafx.stage.Stage;
  *
  */
 public class MenuPrincipalControleur {
-	private static Stage demarrerCommande;
-    private ConnexionControleur parent;
-    private ParametresControleur parentParametres;
-    
-    @FXML
-    private Label infoCompteLbl;
+	@FXML
+	private Label infoCompteLbl;
 	@FXML
 	private Button btnAdministration;
-    @FXML
-    private AnchorPane fenetre;
+	private static Stage demarrerCommande;
+	private static Stage stockAdmin;
+	private static Stage stockServeur;
+	private static Stage carte;
+	private static Stage parametres;
+	private static Stage administration;
+
+	ConnexionControleur parent;
+
+	@FXML
+	private AnchorPane fenetre;
 
 	@FXML
 	public void initialize() {
@@ -47,10 +48,11 @@ public class MenuPrincipalControleur {
 			getDemarrerCommande().setScene(new Scene(vueDCommande));
 			getDemarrerCommande().show();
 			getDemarrerCommande().setTitle("Démarrer une nouvelle commande");
-			
+
 			DemarrerCommandeControleur controller = loader.getController();
-            controller.setParent(this);
+			controller.setParent(this);
 		} catch (Exception e) {
+			FonctionsControleurs.alerteErreur("Erreur!", "Une erreur est survenue", "Détails: "+e);
 			e.printStackTrace();
 		}
 	}
@@ -59,14 +61,16 @@ public class MenuPrincipalControleur {
 	public void fenetreStock() {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("../vue/GestionStock.fxml"));
-			Parent gestionStock = (Parent) loader.load();
-			fenetre.getChildren().setAll(gestionStock);
-			
+			Parent vueGestionStock = (Parent) loader.load();
+			setStockAdmin(new Stage());
+			getStockAdmin().setScene(new Scene(vueGestionStock));
+			getStockAdmin().show();
+			getStockAdmin().setTitle("Gestion du stock");
 
 			GestionStockAdminController controller = loader.getController();
 			controller.setParent(this);
 		} catch (Exception e) {
-			FonctionsControleurs.alerteErreur("Erreur d'éxécution", "Une erreur est survenue", "Détails: " + e);
+			FonctionsControleurs.alerteErreur("Erreur!", "Une erreur est survenue", "Détails: "+e);
 			e.printStackTrace();
 		}
 	}
@@ -75,68 +79,76 @@ public class MenuPrincipalControleur {
 	public void fenetreCarte() {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("../vue/Menu.fxml"));
-			Parent menuClient = (Parent) loader.load();
-			fenetre.getChildren().setAll(menuClient);
+			Parent vueMenu = (Parent) loader.load();
+			setCarte(new Stage());
+			getCarte().setScene(new Scene(vueMenu));
+			getCarte().show();
+			getCarte().setTitle("Menu");
 
 			MenuControleur controller = loader.getController();
 			controller.setParent(this);
 		} catch (Exception e) {
-			FonctionsControleurs.alerteErreur("Erreur d'éxécution", "Une erreur est survenue", "Détails: " + e);
+			FonctionsControleurs.alerteErreur("Erreur!", "Une erreur est survenue", "Détails: "+e);
 			e.printStackTrace();
 		}
 	}
 
-
 	@FXML
 	public void fenetreParametres() {
 		try {
-			Locale locale = new Locale("fr", "FR");
-			ResourceBundle bundle = ResourceBundle.getBundle("gestionbrb/language/Language_fr",locale);
-			FXMLLoader loader = new FXMLLoader(getClass().getResource("../vue/Parametres.fxml"), bundle);
-			Parent menuParametres = (Parent) loader.load();
-			fenetre.getChildren().setAll(menuParametres);  
-				
-				 
-			ParametresControleur controller = loader.getController();
-			controller.setParent(this);
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("../vue/Menu.fxml"));
+		Parent vueParametre = (Parent) loader.load();
+		setParametres(new Stage());
+		getParametres().setScene(new Scene(vueParametre));
+		getParametres().show();
+		getParametres().setTitle("Paramètres");
+
+		ParametresControleur controller = loader.getController();
+		controller.setParent(this);
 		} catch (Exception e) {
-			FonctionsControleurs.alerteErreur("Erreur d'éxécution", "Une erreur est survenue","Détails: "+e);
+			FonctionsControleurs.alerteErreur("Erreur!", "Une erreur est survenue", "Détails: "+e);
 			e.printStackTrace();
-			
 		}
 	}
 
 	@FXML
 	public void fenetreAdministration() {
+
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("../vue/Administration.fxml"));
-			Parent menuClient = (Parent) loader.load();
-			fenetre.getChildren().setAll(menuClient);
+			Parent vueAdministration= (Parent) loader.load();
+			setAdministration(new Stage());
+			getAdministration().setScene(new Scene(vueAdministration));
+			getAdministration().show();
+			getAdministration().setTitle("Administration");
 
 			AdministrationControleur controller = loader.getController();
 			controller.setParent(this);
 		} catch (Exception e) {
-			FonctionsControleurs.alerteErreur("Erreur d'éxécution", "Une erreur est survenue", "Détails: " + e);
+			FonctionsControleurs.alerteErreur("Erreur!", "Une erreur est survenue", "Détails: "+e);
 			e.printStackTrace();
 		}
-	}
 	
+
+	}
+
 	@FXML
 	public void deconnexion() {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("../vue/Connexion.fxml"));
 			Parent menuConnexion = (Parent) loader.load();
 			fenetre.getChildren().setAll(menuConnexion);
-			
+
 			ConnexionControleur controller = loader.getController();
 			controller.setMainApp(this);
 		} catch (Exception e) {
-			FonctionsControleurs.alerteErreur("Erreur d'éxécution", "Une erreur est survenue","Détails: "+e);
+			FonctionsControleurs.alerteErreur("Erreur d'éxécution", "Une erreur est survenue", "Détails: " + e);
 			e.printStackTrace();
 		}
 	}
 
-	// getters & setters utiles pour gérer la fenetre depuis les controleurs associés 
+	// getters & setters utiles pour gérer la fenetre depuis les controleurs
+	// associés
 	public static Stage getDemarrerCommande() {
 		return demarrerCommande;
 	}
@@ -145,12 +157,54 @@ public class MenuPrincipalControleur {
 		MenuPrincipalControleur.demarrerCommande = demarrerCommande;
 	}
 
-	
+	public static Stage getStockAdmin() {
+		return stockAdmin;
+	}
+
+	public static void setStockAdmin(Stage stockAdmin) {
+		MenuPrincipalControleur.stockAdmin = stockAdmin;
+	}
+
+	public static Stage getStockServeur() {
+		return stockServeur;
+	}
+
+	public static void setStockServeur(Stage stockServeur) {
+		MenuPrincipalControleur.stockServeur = stockServeur;
+	}
+
+	public static Stage getCarte() {
+		return carte;
+	}
+
+	public static void setCarte(Stage carte) {
+		MenuPrincipalControleur.carte = carte;
+	}
+
+	public static Stage getParametres() {
+		return parametres;
+	}
+
+	public static void setParametres(Stage parametres) {
+		MenuPrincipalControleur.parametres = parametres;
+	}
+
+	public static Stage getAdministration() {
+		return administration;
+	}
+
+	public static void setAdministration(Stage administration) {
+		MenuPrincipalControleur.administration = administration;
+	}
+
 	/**
-	 * Défini connexionProfil comment parent quand on accède au menu principal depuis la page de connexion
+	 * Défini connexionProfil comment parent quand on accède au menu principal
+	 * depuis la page de connexion
+	 * 
 	 * @param connexionProfil
 	 */
 	public void setParent(ConnexionControleur connexionProfil) {
 		this.parent = connexionProfil;
 	}
+
 }
